f49234a854e1cca335dafe66199c44be4a208080
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-12-07 03:10:48 +0300

    v1.1

--- index.js
+++ index.js
@@ -1,54 +1,61 @@
 import express from "express";
 import multer from "multer";
+import axios from "axios";
 import sizeOf from "image-size";
 import sharp from "sharp";
-// import fs from "fs";
-import axios from "axios";
 
 const app = express();
 
-const img = multer({
-  dest: "./img",
-});
+const img = multer({dest: "./img"});
 
 app
 
-  .get("/makeimage", (r) => {
-    const width = parseInt(r.query.width);
-    const height = parseInt(r.query.height);
+  .get("/makeimage", (req, res) => {
+    const width = parseInt(req.query.width);
+    const height = parseInt(req.query.height);
+    const path = "./img/result.png";
     sharp("./img/img1.jpeg")
       .resize(width, height)
-      .toFile("./img/result.png", (err, info) => {
-        r.res.download("./img/result.png");
-      });
+      .toFile(path, () => {res.download(path)});
   })
 
-  .get("/wordpress/", async (req, res, next) => {
+  .get("/wordpress/", async (req, res) => {
     const content = req.query.content;
-    const response = await axios.post(
-      "https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token",
-      { username: "gossjsstudent2017", password: "|||123|||456" }
-    );
+    const response = await axios
+        .post(
+            "https://wordpress.kodaktor.ru/wp-json/jwt-auth/v1/token",
+            {
+                username: "gossjsstudent2017",
+                password: "|||123|||456"
+            }
+            );
     const token = response.data.token;
-    const WPresponse = await axios.post(
-      `https://wordpress.kodaktor.ru/wp-json/wp/v2/posts/`,
-
-      { title: "chiziwe", content, status: "publish" },
-      {
-        headers: { Authorization: `Bearer ${token}` },
-      }
-    );
-    res.send(WPresponse.data.id + "");
+    const wp_response = await axios
+        .post
+            (`https://wordpress.kodaktor.ru/wp-json/wp/v2/posts/`,
+            {
+                title: "chiziwe",
+                content,
+                status: "publish"
+            },
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+    res.send(wp_response.data.id);
   })
 
-  .post("/size2json", img.single("image"), async (req, res) => {
-    const tempPath = req.file.path;
-    sizeOf(tempPath, function (err, dimensions) {
-        res.send({width: dimensions.width, height: dimensions.height});
+  .post("/size2json", img.single("image"), (req, res) => {
+    const path = req.file.path;
+    sizeOf(path, function (dimensions) {
+        res.send(
+            {
+                width: dimensions.width,
+                height: dimensions.height
+            });
     });
   })
 
-  .all("/login", (r) => r.res.send("chiziwe"))
+  .all("/login", (req, res) => res.send("chiziwe"))
 
   .listen(process.env.PORT || 3000, () => {
     console.log("Привет, я работаю!");
